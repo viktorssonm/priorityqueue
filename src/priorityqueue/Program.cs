1bf26d8b428278b7379c1cb22d10ce9b293dd001
Magnus Viktorsson <m.viktorsson@gmail.com>
2020-11-17 10:18:19 +0100

    initial setup

---
+++ src/priorityqueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace priorityqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
